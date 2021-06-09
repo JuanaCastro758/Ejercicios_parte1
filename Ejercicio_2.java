17935bff619294b8f3d969508cb35c13f49297ed
JuanaCastro758 <juanaazucenacastrojimenez@gmail.com>
2021-06-09 09:36:43 -0600

    se agrega segundo ejercicio, lenguaje java

---
+++ Ejercicio_2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Ejercicio_2{
+	public static void main(String[] args){
+		String p,c;
+		Scanner scanner =new Scanner(System.in);
+		System.out.print("Ingresa el pais: ");
+		p=scanner.nextLine();
+		System.out.print("Ingrese la capital: ");
+		c=scanner.nextLine();
+		System.out.print(c+" es la capital de "+p);
+	}
+}
