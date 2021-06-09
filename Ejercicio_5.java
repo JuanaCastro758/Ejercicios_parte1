4c77af33e16d0419f991e7993e212d7cc3b0a87d
JuanaCastro758 <juanaazucenacastrojimenez@gmail.com>
2021-06-09 09:43:09 -0600

    Se agrega ejercicio 5, lenguaje java

---
+++ Ejercicio_5.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Ejercicio_5{
+	public static void main(String[] args){
+		int n,s;
+		Scanner scanner =new Scanner(System.in);
+		System.out.print("Ingresa numero: ");
+		n=scanner.nextInt();
+		s=n+1;
+		System.out.print("El numero siguiente es "+s);
+	}
+}
